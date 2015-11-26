9a555943c0d23794fcd9fdec201d50cb081c9228
Stepan Vardanyan <styopdev@hotmail.com>
2015-11-27 01:41:41 +0400

    test

--- app/app.js
+++ app/app.js
@@ -62,22 +62,22 @@ app.use(function(err, req, res, next) {
 });
 
 
-var db = mongoose.createConnection('mongodb://aaaaqqqqqq44444:aaaaqqqqqq44444@ds059644.mongolab.com:59644/heroku_x6rx9k5d', function(err){
-  if (err)  {
-    throw err;
-  }
-});
-db.once('open', function(callback) {
-    var MongoStore = require('express-session-mongo');
-    app.use(session({ store: new MongoStore(),
-      genid: function(req) {
-          return uuid.v1();
-      },
-      secret : 'club admin secret key for session',
-      resave :false,
-      saveUninitialized :false
-    }));
-    })
+// var db = mongoose.createConnection('mongodb://aaaaqqqqqq44444:aaaaqqqqqq44444@ds059644.mongolab.com:59644/heroku_x6rx9k5d', function(err){
+//   if (err)  {
+//     throw err;
+//   }
+// });
+// db.once('open', function(callback) {
+//     var MongoStore = require('express-session-mongo');
+//     app.use(session({ store: new MongoStore(),
+//       genid: function(req) {
+//           return uuid.v1();
+//       },
+//       secret : 'club admin secret key for session',
+//       resave :false,
+//       saveUninitialized :false
+//     }));
+//     })
 
 app.listen(process.env.PORT || 3000);
 
